541e5b2b63d70627defb698ac6634e597477f640
Alexis Montes <montes.alexis25@gmail.com>
2020-12-27 00:32:50 -0500

    updated rest methods to use moongose

--- routes/switches.js
+++ routes/switches.js
@@ -1,27 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-var database = require('../server/mongoController');
+//var database = require('../server/mongoController');
+
+let switches = require('../database/models/switches');
+
+const ObjectId = require('mongodb').ObjectID;
 
 /*
     GET route for getting all switch data from database
 */
 router.get('/', function(req, res, next) {
-    //res.send("Test");
-    database.getAllSwitches().then((results) => {
-        res.send(results)
-    })
-    //res.send(());
+
+    switches.find()
+        .then((results) => {
+            res.send(results);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 })
 
 /*
     GET route for getting specific data for a specific switch from database
 */
 router.get('/:id', function(req, res, next){
-    console.log(req.params.id)
+    let objectId = new ObjectId(req.params.id);
+    /*
     database.getSwitchId(req.params.id).then((results) => {
         res.send(results);
     })
+    */
+    switches.find({_id: objectId})
+        .then((results) => {
+            res.send(results);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
     //res.send();
 })
 
