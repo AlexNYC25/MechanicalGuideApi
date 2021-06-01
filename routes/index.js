366e158dfd719a0564104f1d0509e87255654be6
Alexis Montes <montes.alexis25@gmail.com>
2021-05-31 21:13:58 -0400

    added 404 handling in root path

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('mainIntro', {layout: 'main'})
 });
+
+// fall back 404 error
+router.get('/*', function(req, res, next) {
+  res.status(400);
+  res.render('error', {layout: 'main'})
+})
 
 module.exports = router;
