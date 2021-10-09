ac74ed269bb4a8bab51ad193cc6f2fbf6aff146c
Alexis Montes <montes.alexis25@gmail.com>
2021-10-09 00:13:17 -0400

    removed oid 404 route

--- app.js
+++ app.js
@@ -43,10 +43,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 // connect to database
 mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then((result) => {
-    console.log("connected to database");
+    console.log("Connected to database, web API is online");
   })
   .catch((err) => {
-    console.log("There was some sort of error in connecting to the database");
+    console.log("There was some sort of error in connecting to the database:");
+    console.log(err);
   });
 
 // set up routes with the appropriate routers
@@ -54,30 +55,5 @@ app.use('/users', usersRouter);
 app.use('/switches', switchRouter);
 app.use('/documentation', documentationRouter);
 app.use('/', indexRouter);
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-/*
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  /*
-  res.status(err.status || 500);
-  res.render('error', { 
-    post: {number: 25}
-  });
-  
-  res.send("in error handler")
-  
-});
-*/
 
 module.exports = app;
