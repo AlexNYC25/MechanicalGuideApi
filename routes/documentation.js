943db004925721b744ab72ed9bd25190f71ed35e
Alexis Montes <montes.alexis25@gmail.com>
2021-10-10 14:18:47 -0400

    commented documentation routes

--- routes/documentation.js
+++ routes/documentation.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-
-
+// get documentation page related to switch endpoint routes
 router.get('/switch', function(req, res, next) {
     res.render('switch', {layout: 'main'});
 })
 
+// get table of contents page for documentation
 router.get('/', function(req, res, next) {
     res.render('documentation', {layout: 'main'})
 })
